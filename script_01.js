e1b55abeafe3f16190b255d5c0afefca4291ee3b
miche22e28 <mbergamasco11@googlemail.com>
2023-07-17 10:42:55 +0200

    function ausgabeName

--- script_01.js
+++ script_01.js
@@ -3,3 +3,41 @@
 /***** Funktionen 01 *****/
 // 1. Kapselung von Codeblöcken
 
+
+
+// Funkionsrumpf (body) | callee
+// Funktionsdeklaration
+// function test()
+// {
+//     console.log("Hallo Tobias!");
+// }
+
+// Funktionsrumpf (call)
+// test();
+
+/***** Funktion 02a *****/
+// 2a. Parametrisierung + Datenbübergabe von INNEN
+
+
+// ausgabeNamen();
+
+// function ausgabeNamen() {
+    // console.log("Hallo Tobias!");
+    // console.log("Hallo Cagri!");
+    // console.log("Hallo Yasmeen!");
+// }
+
+// ausgabeName();
+
+// function augabeName() {
+
+//     let firstName = "Nico";
+//     console.log("Hallo " + firstName +"!");
+// }
+// Fehler --> SCOPE! (Gültigkeit von Variablen)
+// console.log(firstName);
+
+// innerhalb der Funktion sichtbar | außerhalb der Funktion ungültig
+
+
+
