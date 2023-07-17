01732bc08f6cbe8a43cdad784eba3a664438f1f9
miche22e28 <mbergamasco11@googlemail.com>
2023-07-17 12:42:46 +0200

    string composing

--- script_01.js
+++ script_01.js
@@ -42,13 +42,54 @@
 /***** Funktionen 02b *****/
 // 2b. Parametrisierung + Datenübergabe von AUSSEN
 
-ausgabeNamen2("Sohrab"); // call + Argument(e)
-ausgabeNamen2("Nico");
-ausgabeNamen2("Yasmenn");
+// ausgabeNamen2("Sohrab"); // call + Argument(e)
+// ausgabeNamen2("Nico");
+// ausgabeNamen2("Yasmenn");
 // Argfumente sind Daten für Parameter
 
 // Funktion mit Parameter(n)
-function ausgabeNamen2 (firstName) {
-    console.log("Hallo " + firstName +"!");
+// function ausgabeNamen2 (firstName) {
+//     console.log("Hallo " + firstName +"!");
+// }
+
+/***** Funktionen 02c *****/
+// 2c. Mehrere Parameter / Argumente
+
+// ausgabeNamenParams("Max","Mütze");
+// ausgabeNamenParams(prompt("Vorname?"),prompt("Name?"));
+
+// function ausgabeNamenParams(firstName, familyName) {
+//     console.log("Hallo " + firstName + " " + familyName + "!");
+// }
+
+/***** Funktionen 03a *****/
+// 03a. Vorbereitung -Trennen der Funktionalitäten
+// Postulat: one function = one job (uncle Bob)
+// SRP single responsibility principle
+
+// ausgabeNamenParams("Max","Mütze");
+
+// function ausgabeNamenParams(firstName, familyName) 
+
+/***** Funktionen 03b *****/
+// 03b Funktionalitäten --> Funktionen| return
+
+// 1. Funktionalität: string composing
+
+ output(getString("Max","Mütze"))
+ output(getString(prompt("Vorname?"),prompt("Name?")));
+
+ function getString(firstName, familyName) {
+    const GAP = " ";  // screaming snake case
+    let outputStr = "Hallo" + GAP + firstName + GAP + familyName + "!";
+    return outputStr; // daten werden an den Ort des calls geschickt
 }
 
+
+// 2. Funktionalität: string output
+// output("Hi");
+// output(2);
+// output(true);
+function output(outputData) {
+    console.log(outputData);
+}
